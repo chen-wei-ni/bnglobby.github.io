20c21c7c0e5c12479ed0680df67d1e1afa56748c
chen-wei-ni <bcg2135@gmail.com>
2024-07-10 16:34:16 +0800

    565

--- js/vs/eventPopup.js
+++ js/vs/eventPopup.js
@@ -7,10 +7,8 @@ openBtn.addEventListener("click", (e) => {
     document.body.style.overflow = "hidden"
 })
 closeBtn.addEventListener("click", () => {
-    popup.animate(scaleAnimate2, scaleTiming);
-    setTimeout(() => {
-        popup.style.display = "none";
-    }, 100)
+
+    popup.style.display = "none";
     document.body.style.overflow = "";
 })
 
@@ -18,10 +16,7 @@ const scaleAnimate = [
     { transform: 'scale(0)' },
     { transform: ' scale(1)' },
 ];
-const scaleAnimate2 = [
-    { transform: 'scale(1)' },
-    { transform: ' scale(0)' },
-];
+
 const scaleTiming = {
     duration: 100,
     iterations: 1,
