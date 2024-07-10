1872462a558e29e2df0f23e324dbd0e9d4f69467
chen-wei-ni <bcg2135@gmail.com>
2024-07-10 15:12:38 +0800

    loght box

--- js/vs/eventPopup.js
+++ js/vs/eventPopup.js
@@ -1,36 +1,41 @@
 const openBtn = document.querySelector(".btn");
 const closeBtn = document.querySelector(".close-btn");
 const popup = document.querySelector(".event-desc");
-const content = document.querySelector(".content")
-openBtn.addEventListener("click", (e) => {
-    popup.style.display = "grid";
-    popup.animate(scaleAnimate, scaleTiming);
-    lenis.stop();
-})
-closeBtn.addEventListener("click", () => {
-    popup.style.display = "none";
-    document.body.style.overflow = "";
-    lenis.start();
-})
+// openBtn.addEventListener("click", (e) => {
+//     popup.style.display = "grid";
+//     popup.animate(scaleAnimate, scaleTiming);
+//     document.body.style.overflow = "hidden"
+// })
+// closeBtn.addEventListener("click", () => {
+//     popup.style.display = "none";
+//     document.body.style.overflow = "";
+// })
 
-const scaleAnimate = [
-    { transform: 'scale(0)' },
-    { transform: ' scale(1)' },
-];
-const scaleTiming = {
-    duration: 50,
-    iterations: 1,
-}
-const lenis = new Lenis()
-
-lenis.on('scroll', (e) => {
-    console.log(e)
-})
-
-function raf(time) {
-    lenis.raf(time)
-    requestAnimationFrame(raf)
-}
+// const scaleAnimate = [
+//     { transform: 'scale(0)' },
+//     { transform: ' scale(1)' },
+// ];
+// const scaleTiming = {
+//     duration: 50,
+//     iterations: 1,
+// }
 
 
-requestAnimationFrame(raf)
+$(function () {
+    $('.open-popup-link').magnificPopup({
+        type: 'inline',
+        midClick: true,
+        closeBtnInside: true,
+        fixedContentPos: true,
+        mainClass: 'mfp-with-anim',
+        removalDelay: 500,
+        callbacks: {
+            beforeOpen: function () {
+                this.st.mainClass = this.st.el.attr('data-effect');
+            }
+        }
+    });
+    $('.popup-close').click(function () {
+        $.magnificPopup.close();
+    });
+});
