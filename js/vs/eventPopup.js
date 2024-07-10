053d2e24a9302d74a40914d9264ade973f9472cb
chen-wei-ni <bcg2135@gmail.com>
2024-07-10 14:18:45 +0800

    lenis

--- js/vs/eventPopup.js
+++ js/vs/eventPopup.js
@@ -19,4 +19,15 @@ const scaleTiming = {
     duration: 50,
     iterations: 1,
 }
+const lenis = new Lenis()
 
+lenis.on('scroll', (e) => {
+    console.log(e)
+})
+
+function raf(time) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
